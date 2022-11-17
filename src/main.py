c0edd08c630a3cdf8d51e03b4b47fd8235439da9
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2022-11-18 02:07:16 +0600

    added exceptions. added art. fixed some code.

--- src/main.py
+++ src/main.py
@@ -9,15 +9,30 @@ import PIL
 from PIL import Image
 from rich.console import Console
 from rich.progress import Progress
-import time
+from rich import print
 
 selectedFiles = []
 files_selected = 0
 files_skipped = 0
 files_detected = 0
+files_converted = 0
 
 console = Console()
 
+
+def text_art():
+    """
+    Welcoming text artwork in ASCII.
+    Remember setting the encoding to utf8 or the image will not be read.
+    """
+    try:
+        with open('src/assets/art.txt', encoding="utf8") as text:
+            lines = text.read()
+            console.print(f'[green][blink]{lines}[/]', justify="center")
+            console.print("Made by [link=https://www.twitter.com/Black_2_white]@Black_2_white[/link]!")
+    except (FileNotFoundError, UnicodeDecodeError):
+        pass # pass the error if found. Best needed for old terminals
+        
 
 def scan_dir(target_dir):
     """
@@ -44,19 +59,33 @@ def scan_dir(target_dir):
     console.print(f"total selected files: {files_selected}")
 
 
+
+
 def saving_images(file_list , saving_dir):
+    
+    global files_converted
     """
     saves file image from a given directory.
     """
-    with Progress(transient=True) as progress:
-        task_1 = progress.add_task("[blue]Converting files...", total=len(file_list)) # total files are the selected files
+    try:
+        with console.screen(): # create alternate screen. See rich-doc
+            with Progress(transient=True) as progress:
+                task_1 = progress.add_task("[blue]Converting files...", total=len(file_list)) # total files are the selected files
 
-        for file in file_list:
-            img = Image.open(f"{target_dir}/{file}")
-            img.save(f"{saving_dir}/{file}.png")
-            progress.update(task_1, advance=1) # advance progress after 1 file is converted
 
-    console.print(f"Files saved at [i][blue][blink2]{saving_dir}[/]")
+                for file in file_list:
+                    img = Image.open(f"{target_dir}/{file}")
+                    img.save(f"{saving_dir}/{file}.png")
+                    progress.update(task_1, advance=1) # advance progress after 1 file is converted
+                    files_converted+=1
+
+        console.print(f"""Total files converted (JPG => PNG) : {files_converted}
+                      Files saved at [i][blue][blink2]{saving_dir}[/]
+                      """)
+    except KeyboardInterrupt:
+        console.print("[red]Warn[/]. Program force-exit over converting process. Chances of corrupt-files is possible.")
+
+
 
 
 
@@ -80,18 +109,18 @@ def script_Runtime(target_dir):
             break
 
         else: # if the response is not expected, continue loop
-            console.print(f"[red]Warning: Not a valid response[/]\n[red]Valid response either y or n. Got [/]{userChoice}\n[yellow]Try again[/]")
+            console.print(f"[red]Warn: Not a valid response[/]\n[red]Valid response either y or n. Got [/]{userChoice}\n[yellow]Try again[/]")
 
 
 def main(target_dir):
     try: 
         scan_dir(target_dir) # running this causes the files to be updated
-    except FileNotFoundError: 
-        console.print("[red]Warning![/] No such file folder exists.")
+    except FileNotFoundError: # notify user if they inserted a wrong target directory
+        console.print("[red]Warn![/] No such folder exists.")
         return True
     
     if len(selectedFiles) == 0: # if length of selected files is 0, continue loop.
-        console.print("[red]Warning: No images found to convert to png[/]\n[yellow]Try Again.[/]")
+        console.print("[red]Warn: No images found to convert to png[/]\n[yellow]Try Again.[/]")
 
     elif len(selectedFiles) > 0: # if length of selected files is not 0, break loop after saving
         console.print("[green]Images found")
@@ -99,9 +128,15 @@ def main(target_dir):
         return False
     
 try:
-    while True:
+    text_art()
+    while True: ## main loop
         target_dir = input("Enter your directory here: ")
         if main(target_dir) == False: break # break loop if the function is completed. 
 except KeyboardInterrupt:
     console.print("\n[blue]Program exiting now....[/]")
     sys.exit()
+except ModuleNotFoundError:
+    console.print('''
+                  Program will not run because the dependencies are incomplete.
+                  Please download dependencies and check them correctly. You can download them from the Github Repo.
+                  ''')
